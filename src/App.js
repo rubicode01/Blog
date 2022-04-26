c9cb742bc10e7256d6609321463ff475be4367b9
Paul Ludwigs <paul.ludwigs@web.de>
2022-04-26 16:14:24 +0200

    Implement contentful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ContentfulClientApi } from "contentful";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const contentful = require("contentful");
+
+  const client = contentful.createClient({
+    space: process.env.REACT_APP_SPACE_ID,
+    environment: "master", // defaults to 'master' if not set
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  });
+
+  client
+    .getEntries()
+    .then((response) => console.log(response.items))
+    .catch(console.error);
+
+  return <div className="App"></div>;
 }
 
 export default App;
