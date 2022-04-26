0ce53c05f80a28184e4a3c71b1299df7b5a0e27c
Ivana <rubicode01@gmail.com>
2022-04-26 16:21:41 +0200

    add env

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1>The 4th Blog</h1>
+
+
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
