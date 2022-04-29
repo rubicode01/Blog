1916b07a83e04d8887c0ed139b24051293bf6a0d
Paul Ludwigs <paul.ludwigs@web.de>
2022-04-29 16:49:54 +0200

    Remove Router and Home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ContentfulClientApi } from "contentful";
-import Router from "./components/Router";
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
+import Post from "./components/Post";
 
 function App() {
-
-
   const [posts, setPosts] = useState([]);
 
   //Contentful
@@ -21,28 +20,39 @@ function App() {
   });
 
   useEffect(() => {
-      client
-        .getEntries()
-        .then((response) => {
-          setPosts(response.items);
-          
-        })
-        
-        .catch(console.error);
+    client
+      .getEntries()
+      .then((response) => {
+        setPosts(response.items);
+      })
 
-  },[])
-
+      .catch(console.error);
+  }, []);
 
   return (
-  <div className="App">
-    <NavBar/>
- 
-  {posts ? 
-    <Router posts={posts} /> :
-    <h2>Loading...</h2>
-  
-  }
-  </div>);
+    <div className="App">
+      <NavBar />
+
+      {posts ? (
+        <>
+          {posts.map((post) => (
+            <>
+              <h3>{post.fields.title}</h3>
+              <p className="details">{post.fields.author}</p>
+              <p className="details">{post.fields.date}</p>
+              <p>{post.fields.content}</p>
+              <img src={post.fields.picture.fields.file.url} />
+            </> //Post ID: post.fields.sys.id
+          ))}
+        </>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+      <Routes>
+        <Route path="post/:id" element={<Post />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
