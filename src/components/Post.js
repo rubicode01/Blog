bcdf027a9e491260912a95336c4ad1dde90666f8
Karim Derrouiche <k.derrouiche1988@gmail.com>
2022-05-03 18:09:57 +0200

    test

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,15 +2,29 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function Post({ posts }) {
-  console.log(posts);
+  //console.log(posts);
   const { id } = useParams();
   const post = posts.find((post) => id === post.sys.id);
+  console.log(post)
+  const pictureUrl = post.fields.picture.fields.file.url;
+  const date = post.fields.date;
+
+  /*const getDateFormat = (date) =>{
+    let days = date.getDate();
+    let months = date.getMonth();
+    let years = date.getFullYear();
+    let dateFormat = days + ":" + months + ":" + years;
+    return dateFormat;
+  }
+*/
   return (
     <div>
+      <h1>{post.fields.title}</h1>
+      <p><h5>{post.fields.author} : </h5><h6>{date}</h6></p>
       <div>
         <img
           fluid
-          src="https://www2.tuhh.de/zll/wp-content/uploads/placeholder.png"
+          src={pictureUrl} alt={post.fields.title}
         />
       </div>
       <div>
