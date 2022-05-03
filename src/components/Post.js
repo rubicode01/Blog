c9cba2020a5a02c2141718a57def264d17e60d21
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-03 16:17:50 +0200

    Basic post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,33 +1,27 @@
 import React from "react";
-import bootstrap from "bootstrap";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import { Image, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 
-function Post({posts}) {
-  console.log(posts)
-  const {id} = useParams();
-  const post = posts.find(post => id === post.sys.id)
+function Post({ posts }) {
+  console.log(posts);
+  const { id } = useParams();
+  const post = posts.find((post) => id === post.sys.id);
   return (
-    <Container fluid className="big-card">
-    <Row>
-      <Col lg>
-      <Image fluid
-        className="big-card-img"
-        src="https://www2.tuhh.de/zll/wp-content/uploads/placeholder.png"
-      />
-      </Col>
-      <Col lg className="big-card-body">
+    <div>
+      <div>
+        <img
+          fluid
+          src="https://www2.tuhh.de/zll/wp-content/uploads/placeholder.png"
+        />
+      </div>
+      <div>
         <h3>{post.fields.title}</h3>
-      <p>{post.fields.content}</p>
-          <Link to={post.sys.id}><Button variant="outline-primary" className="big-card-button">Zum Artikel</Button></Link>
-      </Col>
-    </Row>
-  </Container>
-  )
+        <p>author: {post.fields.author}</p>
+        <p>date: {post.fields.date}</p>
+        <br></br>
+        <p>{post.fields.content}</p>
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
