c4c03564693ef0e0975d03060f23bf84023307ec
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-05 17:30:41 +0200

    Refactor Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,12 @@
 import React from "react";
 import BigCard from "./BigCard";
-import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
 import SmallCard from "./SmallCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Home({ posts }) {
   //get newest Post(for BigCard) and remove it from posts
   const smallPosts = [...posts];
   const newPost = smallPosts.shift();
-
-  //Example for Accessing post details
-  /*<h3>{post.fields.title}</h3>
-            <p className="details">{post.fields.author}</p>
-            <p className="details">{post.fields.date}</p>
-            <p>{post.fields.content}</p>
-            <img src={post.fields.picture.fields.file.url} />
-        </> //Post ID: post.fields.sys.id*/
 
   return (
     <>
@@ -23,13 +14,13 @@ function Home({ posts }) {
         <Container>
           <Row>
             <Col>
-              <BigCard post={newPost} /> {/*BigCard here*/}
+              <BigCard post={newPost} />
             </Col>
           </Row>
           <Row xs={1} s={2} md={3} style={{ justifyContent: "space-between" }}>
             {smallPosts.map((post) => (
               <Col>
-              <SmallCard key={post.sys.id} post={post}/> {/*SmallCard here*/}
+              <SmallCard key={post.sys.id} post={post}/>
               </Col>
             ))}
           </Row>
