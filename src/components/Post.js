3e3233b6b921475e4d999816046af0925c983491
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-05 17:32:39 +0200

    Refactor Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,7 +6,6 @@ function Post({ posts }) {
   const { id } = useParams();
   const post = posts.find((post) => id === post.sys.id);
   const pictureUrl = post.fields.picture.fields.file.url;
-  //console.log(post)
 
   const date = post.fields.date;
 
@@ -29,37 +28,40 @@ function Post({ posts }) {
 
   return (
     <div>
-      
       <Container className="postBox">
-      
         <Row className="rowdesign">
-          <Col><h2>{post.fields.title}</h2></Col>
-          
-          
+          <Col>
+            <h2>{post.fields.title}</h2>
+          </Col>
         </Row>
-    
         <Row>
-          <Col >
-            <Image className="img-fluid imageRow"
+          <Col>
+            <Image
+              className="img-fluid imageRow"
               align="start"
               src={pictureUrl}
               alt={post.fields.title}
             />
           </Col>
-          </Row>
-          <Row>
+        </Row>
+        <Row>
           <Col>
-          <p className="info">{post.fields.author}</p>
-            <p className="info">{getDateFormat(date)}</p></Col>
+            <p className="info">{post.fields.author}</p>
+            <p className="info">{getDateFormat(date)}</p>
+          </Col>
         </Row>
         <Row className="rowdesign">
           <Col xs={12} lg={6}>
-          <p className="text" style={{textAlign: "justify"}}>{sliceContent(content)[0]}</p>
+            <p className="text" style={{ textAlign: "justify" }}>
+              {sliceContent(content)[0]}
+            </p>
           </Col>
-        <Col>
-          <p className="text" style={{textAlign: "justify"}}>{sliceContent(content)[1]}</p>
+          <Col>
+            <p className="text" style={{ textAlign: "justify" }}>
+              {sliceContent(content)[1]}
+            </p>
           </Col>
-          </Row>
+        </Row>
       </Container>
     </div>
   );
