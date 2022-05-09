40b2910c39347c311ff43ab65b2e5b8bcd2dbef8
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-09 16:39:08 +0200

    Add logo to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Container, Navbar, Link } from 'react-bootstrap'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Navbar, Image } from "react-bootstrap";
 
 const NavBar = () => {
   return (
     <div className="navBG">
-<Navbar>
-    <Container>
-      <Navbar.Brand>
-        <img
-          alt="Blog4Group"
-          src="/img/B4G_Logo.png"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-        />{' '}
-       <Link to="/"><h1 >Blog 4 Groups</h1></Link>
-      </Navbar.Brand>
- <Container>
+      <Navbar>
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">
+              {" "}
+              <img
+                alt="Blog4Group"
+                src="./B4G_Logo.png"
+                width="164"
+                height="72"
+                className="d-inline-block align-top logo"
+              />
+            </Link>
+          </Navbar.Brand>
+        </Container>
       </Navbar>
-
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
