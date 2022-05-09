7621edcbd23d8a9b7ef472fce9b9513d6c96b0c3
Ivana <rubicode01@gmail.com>
2022-05-09 16:14:46 +0200

    insert logo and favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Container, Navbar, Link } from 'react-bootstrap'
 
 
 const NavBar = () => {
   return (
     <div className="navBG">
-    <Link to="/"><h1 >Blog 4 Groups</h1></Link>
-  
+<Navbar>
+    <Container>
+      <Navbar.Brand>
+        <img
+          alt="Blog4Group"
+          src="/img/B4G_Logo.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+       <Link to="/"><h1 >Blog 4 Groups</h1></Link>
+      </Navbar.Brand>
+ <Container>
+      </Navbar>
+
     </div>
   )
 }
