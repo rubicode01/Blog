ea50459a6e52e1bc98b9e9061e495e93d29fb0d8
Paul Ludwigs <paul.ludwigs@web.de>
2022-05-22 16:33:46 +0200

    Save values from comment form

--- src/components/CreateComment.js
+++ src/components/CreateComment.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
 function CreateComment() {
+  const [values, setValues] = useState({
+    name: "",
+    comment: "",
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValues({ "name": e.target.name.value, "comment": e.target.comment.value})
+    console.log(values);
+  }
+
   return (
     <div className="comment-form">
       <h3 className="comment-form-title">Write a Comment</h3>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-2" controlId="formBasicEmail">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="input" placeholder="Enter Name" />
+          <Form.Control name="name" type="input" placeholder="Enter Name" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Comment</Form.Label>
-          <Form.Control as="textarea" rows={3} />
+          <Form.Control name="comment" as="textarea" rows={3} />
         </Form.Group>
         <Button className="btn-card" variant="secondary" type="submit">
               Submit
